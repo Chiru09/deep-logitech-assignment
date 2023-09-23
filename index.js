7e652c265deab54d26e1a318f4b0f6fa43220b44
Chiranjiv Kumar <chiranjivkumargc@gmail.com>
2023-09-23 17:51:15 +0530

    Done

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const scrapper = require('./scrapper')
 
 
 const server = http.createServer(async (req, res) => {
-  if (req.url === '/getTimeStories' && req.method === 'GET') {
+    if (req.url === '/getTimeStories' && req.method === 'GET') {
 
-    const data = await scrapper.getDataFromTime();
+        const data = await scrapper.getDataFromTime();
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(data.slice(-6)));
-  } else {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(data.slice(-6)));
+    } else {
 
-    // Handle other requests with a 404 Not Found response
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+        // 404 if Not Found response
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
 
 
-// Start the server
+// Start
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- scrapper.js
+++ scrapper.js
@@ -1,51 +1,50 @@
 const https = require('https');
 
-// URL of the web page to scrape
 const url = 'https://time.com/';
 
-// Send an HTTP GET request to the URL
+
 function getDataFromTime() {
-  return new Promise((resolve, reject) => {
-    https.get(url, (response) => {
-      let data = '';
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
 
-      // Read the response data
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
+            // get html content
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
 
-      // Process the data when the response ends
-      response.on('end', () => {
-        if (response.statusCode === 200) {
-          // Process the HTML content and resolve the promise with the articles
-          const articles = extractArticleTitles(data);
-          resolve(articles);
-        } else {
-          console.log('Failed to retrieve web page.');
-          reject(new Error('Failed to retrieve web page.'));
-        }
-      });
-    }).on('error', (error) => {
-      console.error('Error:', error);
-      reject(error);
+            response.on('end', () => {
+                if (response.statusCode === 200) {
+
+                    const articles = extractArticleTitles(data);
+                    resolve(articles);
+                } else {
+                    console.log('Failed to retrieve web page.');
+                    reject(new Error('Failed to retrieve web page.'));
+                }
+            });
+        }).on('error', (error) => {
+            console.error('Error:', error);
+            reject(error);
+        });
     });
-  });
 }
 
+// extract data from html using regex
 function extractArticleTitles(html) {
-  const regex = /<a\s+href="([^"]+)">\s*<h3[^>]*>([^<]+)<\/h3>/g;
-  let match;
-  const results = [];
+    const regex = /<a\s+href="([^"]+)">\s*<h3[^>]*>([^<]+)<\/h3>/g;
+    let match;
+    const results = [];
 
-  while ((match = regex.exec(html)) !== null) {
-    const title = match[2];
-    const link = "https://time.com" + match[1];
-    results.push({ title, link });
-  }
+    while ((match = regex.exec(html)) !== null) {
+        const link = "https://time.com" + match[1];
+        const title = match[2];
+        results.push({ title, link });
+    }
 
-  return results;
+    return results;
 }
 
 module.exports = {
-  getDataFromTime,
+    getDataFromTime,
 };
